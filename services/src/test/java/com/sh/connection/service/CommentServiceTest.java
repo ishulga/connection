0e445491e39dd0dc58831f9af194bac256890145
ishulga <evg.shulga@gmail.com>
2014-03-11 22:04:46 +0200

    refactored, modify .gitignore

--- services/src/test/java/com/sh/connection/service/CommentServiceTest.java
+++ services/src/test/java/com/sh/connection/service/CommentServiceTest.java
@@ -3,7 +3,6 @@ package com.sh.connection.service;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
